dcc72574868293390ab4f5b6d79c116d4859e409
Xantiros <xantiros@gmail.com>
2019-02-14 14:26:54 +0100

    DataAccess

---
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace DataLibrary.DataAccess
+{
+    public static class SqlDataAccess
+    {
+        public static string GetConnectionString(string connectionName = "MVCDemoDB")
+        {
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+        public static List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection con = new SqlConnection(GetConnectionString()))
+            {
+                return con.Query<T>(sql).ToList();
+            }
+        }
+        public static int SaveData<T>(string sql, T data)
+        {
+            using (IDbConnection con = new SqlConnection(GetConnectionString()))
+            {
+                return con.Execute(sql, data);
+            }
+        }
+
+    }
+}
