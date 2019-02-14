f514d376548ba94d6b8933d6b6cc5561c5554e55
Xantiros <xantiros@gmail.com>
2019-02-14 11:05:35 +0100

    Validation

--- MVCDemo/Models/EmployeeModel.cs
+++ MVCDemo/Models/EmployeeModel.cs
@@ -1,18 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MVCDemo.Models
 {
     public class EmployeeModel
     {
+        [Display(Name = "Employee ID")]
+        [Range(100000, 999999, ErrorMessage = "You need to enter a valid EmployeeId")]
         public int EmployeeId { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "You need to give us your first name.")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "You need to give us your last name.")]
         public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "You need to give us your email address.")]
         public string EmailAddress { get; set; }
+
+        [Display(Name = "Confirm Email")]
+        [DataType(DataType.EmailAddress)]
+        [Compare("EmailAddress", ErrorMessage = "The Email and Confirm Email must match.")]
         public string ConfirmEmail { get; set; }
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "You need to provide a long enough password.") ]
         public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("EmailAddress", ErrorMessage = "Your password and confire password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
