b16e9fb25aad5262f3f8cca4207f18913b7e3831
Xantiros <xantiros@gmail.com>
2019-02-14 14:35:34 +0100

    BusinessLogic

---
+++ DataLibrary/BusinessLogic/EmployeeProcessor.cs
@@ -0,0 +1,31 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System.Collections.Generic;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class EmployeeProcessor
+    {
+        public static int CreateEmployee(int employeeId, string firstName, string lastName, string emailAddress)
+        {
+            EmployeeModel data = new EmployeeModel
+            {
+                EmployeeId = employeeId,
+                FirstName = firstName,
+                LastName = lastName,
+                EmailAddress = emailAddress
+            };
+
+            string sql = @"inset into dbo.Employee (EmployeeId, FirstName, LastName, EmailAddress)
+                        values (@EmployeeId, @Firstname, @LastName, @EmailAddress);";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+        public static List<EmployeeModel> LoadEmployees()
+        {
+            string sql = "select Id, EmployeeId, FirstName, LastName, EmailAddress from dbo.Employee;";
+
+            return SqlDataAccess.LoadData<EmployeeModel>(sql);
+        }
+    }
+}
