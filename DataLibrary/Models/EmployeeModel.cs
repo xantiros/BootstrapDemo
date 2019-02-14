0a3deb33f8595226fe8a042df530ad8041c8390a
Xantiros <xantiros@gmail.com>
2019-02-14 13:34:43 +0100

    EmployeeModel

---
+++ DataLibrary/Models/EmployeeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DataLibrary.Models
+{
+    public class EmployeeModel
+    {//do mapowania mozna uzyc AutoMapera
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
